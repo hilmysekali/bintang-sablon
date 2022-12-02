16f8b2b79e9b449d8a335dcb08f217f4e8e1987e
hilmysekali <zulhilmidharma@gmail.com>
2022-12-02 15:53:17 +0700

    Edit Basename/

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <BrowserRouter basename='/bintang-sablon'>
+    // <BrowserRouter basename='/bintang-sablon'>
+    <BrowserRouter>
       <Provider store={store}>
         <Routes>
           <Route path="/*" element={<App />} />
