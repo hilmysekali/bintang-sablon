6ffd1cab8fe43024f6b9ccb26957f6632b3ee631
hilmysekali <zulhilmidharma@gmail.com>
2022-12-02 16:01:21 +0700

    Change Directory

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    // <BrowserRouter basename='/bintang-sablon'>
-    <BrowserRouter>
+    <BrowserRouter basename='/bintang-sablon'>
       <Provider store={store}>
         <Routes>
           <Route path="/*" element={<App />} />
