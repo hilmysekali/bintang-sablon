c5ea001221eb27ad64703d79c8e3a816dcd79df7
hilmysekali <zulhilmidharma@gmail.com>
2022-12-03 08:45:33 +0700

    Change lagi

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <HashRouter basename='/bintang-sablon'>
+  <HashRouter>
     <Provider store={store}>
       <Routes>
         <Route path="/*" element={<App />} />
